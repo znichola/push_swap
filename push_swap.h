c5d90e51b0ada0446545ec9029a74602aa27061d
znichola <niki.zivkovic@hotmail.com>
2022-11-05 19:52:51 +0100

    foo

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znichola <znichola@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/04 19:23:26 by znichola          #+#    #+#             */
+/*   Updated: 2022/11/04 19:27:09 by znichola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+
+typedef struct s_bt
+{
+	int	data;
+	t_bt	*right_node;
+	t_bt	*left_node;
+} t_bt;
+
+#endif /* push swap*/
