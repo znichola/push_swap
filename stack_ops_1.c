16bd2566e68a4c414e06e4ba1f9507be02131177
znichola <znichola@student.42lausanne.ch>
2022-11-16 19:06:22 +0100

    1.6 for real

--- stack_ops_1.c
+++ stack_ops_1.c
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:58:14 by znichola          #+#    #+#             */
-/*   Updated: 2022/11/16 18:41:23 by znichola         ###   ########.fr       */
+/*   Updated: 2022/11/16 19:06:08 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,54 @@
 
 // wrapper: pa (push a): Take the first element at the top of b
 // and put it at the top of a. Do nothing if b is empty.
-// int	pa(t_stack *s)
-// {
-// 	if (s->r.finish_me != EMPTY)
-// 		return (FAILURE);
-// 	// printf("we have data available\n");
-// 	// for (int i = 0; i < s->size; i++)
-// 	// 	printf("%d\n", s->r.solution[i]);
-// 	// printf("solution pointer:%p n:%d\n", s->r.a, *s->r.a);
-// 	// exit(1);
-// 	// check if push breaks the order
-	
-// 	return (pa_(s));
-// }
-
 int	pa(t_stack *s)
 {
-	if (!(s->r.finish_me == EMPTY || s->r.finish_me == PA))
+	if (s->r.finish_me != EMPTY)
 		return (FAILURE);
-	if (pa_(s) == FAILURE)
-		return (FAILURE);
-	if (s->r.b_hight > 1)
-		s->r.finish_me = PA;
-	else
-		s->r.finish_me = EMPTY;
-	return (SUCCESS);
+	// printf("we have data available\n");
+	// for (int i = 0; i < s->size; i++)
+	// 	printf("%d\n", s->r.solution[i]);
+	// printf("solution pointer:%p n:%d\n", s->r.a, *s->r.a);
+	// exit(1);
+	// check if push breaks the order
+	
+	return (pa_(s));
 }
+
+// int	pa(t_stack *s)
+// {
+// 	if (!(s->r.finish_me == EMPTY || s->r.finish_me == PA))
+// 		return (FAILURE);
+// 	if (pa_(s) == FAILURE)
+// 		return (FAILURE);
+// 	if (s->r.b_hight > 1)
+// 		s->r.finish_me = PA;
+// 	else
+// 		s->r.finish_me = EMPTY;
+// 	return (SUCCESS);
+// }
 
 // wrapper: pb (push b): Take the first element at the top of a
 // and put it at the top of b. Do nothing if a is empty.
-// int	pb(t_stack *s)
-// {
-// 	if (!(s->r.finish_me == EMPTY))
-// 		return (FAILURE);
-// 	return (pb_(s));
-// }
-
 int	pb(t_stack *s)
 {
-	if (!(s->r.finish_me == EMPTY || s->r.finish_me == PB))
+	if (!(s->r.finish_me == EMPTY))
 		return (FAILURE);
-	if (pa_(s) == FAILURE)
-		return (FAILURE);
-	if (s->r.a_hight > 1)
-		s->r.finish_me = PB;
-	else
-		s->r.finish_me = EMPTY;
-	return (SUCCESS);
+	return (pb_(s));
 }
+
+// int	pb(t_stack *s)
+// {
+// 	if (!(s->r.finish_me == EMPTY || s->r.finish_me == PB))
+// 		return (FAILURE);
+// 	if (pa_(s) == FAILURE)
+// 		return (FAILURE);
+// 	if (s->r.a_hight > 1)
+// 		s->r.finish_me = PB;
+// 	else
+// 		s->r.finish_me = EMPTY;
+// 	return (SUCCESS);
+// }
 
 // wrapper: ra (rotate a): Shift up all elements of stack a by 1.
 // The first element becomes the last one.
